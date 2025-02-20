4ebab7a68f450e55fab71001c70f478eaf1324de
Your Name <you@example.com>
2025-02-19 21:57:18 -0700

    awsproject

--- mysql.sh
+++ mysql.sh
@@ -8,6 +8,8 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
+echo "please enter DB password:"
+read -s mysql_root_password
 
 VALIDATE(){
    if [ $1 -ne 0 ]
@@ -41,10 +43,10 @@ VALIDATE $? "Starting mysql-server"
 # VALIDATE $? "Setting up root password"
 
 #BELOW code is used to idempotency nature
-mysql -h db.awsproject.online -uroot -pExpenseApp@1 -e 'show databases;' &>>$LOGFILE
+mysql -h db.awsproject.online -uroot -p${mysql_root_password} -e 'show databases;' &>>$LOGFILE
 if [ $? -ne 0 ]
 then
-    mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOGFILE
+    mysql_secure_installation --set-root-pass ${mysql_root_password} &>>$LOGFILE
     VALIDATE $? "Setting up root password"
 else
     echo -e "Root password is already set...$Y SKIPPING $N"
